7171656d699084606ac39773e74eeb1b09efc105
Raphael9143 <s3jack3s@gmail.com>
2024-10-24 07:18:21 +0700

    [feat] form

--- Week 5/form/index.js
+++ Week 5/form/index.js
@@ -1,6 +1,10 @@
 let defaultText = "Quý vị chưa nhập "
 let emptyText = ""
 let date_regex = /^(((0[1-9]|[12][0-9]|30)[-/]?(0[13-9]|1[012])|31[-/]?(0[13578]|1[02])|(0[1-9]|1[0-9]|2[0-8])[-/]?02)[-/]?[0-9]{4}|29[-/]?02[-/]?([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00))$/
+
+let inputQueries = document.querySelectorAll("input:not([type=radio]):not([type=checkbox])")
+
+
 function isEmpty() {
     if (document.forms['frm']['name'].value == "") {
         document.getElementById('name-error').innerHTML = defaultText + "họ tên"
@@ -43,3 +47,11 @@ function loadFile(event) {
         URL.revokeObjectURL(output.src)
     }
 }
+
+inputQueries.forEach(element => {
+    element.addEventListener('keydown', e => {
+        if (e.keyCode === 13) {
+            let 
+        }
+    })
+})
